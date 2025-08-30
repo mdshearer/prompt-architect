60bc968c9d14b110ba62070ef864152b755cadee
Malcolm Shearer <malcolm@enspiral.com>
2025-08-30 21:17:29 +1200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,30 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'text-optimi-primary',
+    'text-optimi-blue', 
+    'text-optimi-green',
+    'text-optimi-yellow',
+    'text-optimi-gray',
+    'bg-optimi-primary',
+    'bg-optimi-blue',
+    'bg-optimi-green', 
+    'bg-optimi-yellow',
+    'bg-optimi-gray',
+    'border-optimi-primary',
+    'border-optimi-blue',
+    'border-optimi-green',
+    'border-optimi-yellow',
+    'border-optimi-gray',
+    'hover:bg-optimi-primary/80',
+    'hover:bg-optimi-blue/80',
+    'hover:bg-optimi-green/80',
+    'hover:border-optimi-blue',
+    'hover:border-optimi-green',
+    'hover:border-optimi-yellow',
+    'focus:border-optimi-blue'
+  ],
   theme: {
     extend: {
       colors: {
